9fe377a25b84cf6c839b3eb4ce57fc0504b35802
mpeskicev7-netizen <mpeskicev7@gmail.com>
2026-05-07 08:16:37 +0300

    начал делать
    начал делать сохранение в файл и цикл выбора операции

--- Library/Library/Library.cpp
+++ Library/Library/Library.cpp
@@ -1,5 +1,9 @@
 ﻿#include <iostream>
 #include<string>
+#include <fstream>
+#include<vector>
+#include<windows.h>
+
 
 using namespace std;
 
@@ -21,3 +25,18 @@ using namespace std;
 	 string Name;
 	 int Duration;
  };
+
+  void SaveToFile(const vector<Book>& books, const vector<Music>& musics, const vector<Movie>& movies, const string& filename) {
+	  ofstream file(filename);
+
+  }
+
+  int main() {
+	  setlocale(LC_ALL, "ru");
+
+	  char Choice;
+
+	  while (true) {
+		  cout << "Выберите функцию: \n B - Добавить книгу \n M - Добавить музыку \n F - Добавить фильм";
+	  }
+  }
