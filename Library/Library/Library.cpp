a821797c0488ee29402df20415fd1b3929bb6379
mpeskicev7-netizen <mpeskicev7@gmail.com>
2026-05-07 04:23:57 +0300

    библиотека
    добавил классы так на скорую руку

---
+++ Library/Library/Library.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include<string>
+
+using namespace std;
+
+ class Book {
+ public:
+	 string Name;
+	 int Pages;
+
+};
+
+ class Music {
+ public:
+	 string Name;
+	 int Duration;
+ };
+
+ class Movie {
+ public:
+	 string Name;
+	 int Duration;
+ };
