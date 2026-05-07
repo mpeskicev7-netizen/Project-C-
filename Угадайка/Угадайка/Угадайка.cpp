38e4c188ca665d6b083d9b73f5801913bcdc749b
mpeskicev7-netizen <mpeskicev7@gmail.com>
2026-05-07 04:10:21 +0300

    Test

---
+++ Угадайка/Угадайка/Угадайка.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include<ctime>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	srand(time(0));
+
+	int a;
+	int secret = rand() % 100 + 1;
+	int attempts = 0;
+
+	cout << "Я загадал число от 1 до 100 угадай его" << endl;
+
+	while (true) {
+		attempts++;
+		cout << "Попытка: " << attempts << endl;
+		cin >> a;
+
+		if (a < 1 || a > 100) {
+			attempts--;
+			cout << "Ты введ неправильное число" << endl;
+			continue;
+		}
+
+		if (secret == a) {
+			cout << "Угадал за " << attempts << " попыток" << endl;
+			cout << "Моё число " << secret << endl;
+
+			break;
+		}
+
+		else if (secret > a) {
+			cout << "Моё число больше" << endl;
+		}
+
+		else {
+			cout << "Моё число меньше" << endl;
+		}
+	}
+	return 0;
+}
