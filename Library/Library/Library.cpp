4115476524a7b043e837511f3a54c6de2869f8ba
mpeskicev7-netizen <mpeskicev7@gmail.com>
2026-05-10 19:30:04 +0300

    комент
    доделал сохранение в файл начал делать заполнение книг

--- Library/Library/Library.cpp
+++ Library/Library/Library.cpp
@@ -29,15 +29,50 @@ using namespace std;
   void SaveToFile(const vector<Book>& books, const vector<Music>& musics, const vector<Movie>& movies, const string& filename) {
 	  ofstream file(filename);
 
+	  if (!file.is_open()) {
+		  cout << "Не удалось отркыть файл";
+
+		  return;
+	  }
+
+	  file << books.size() << musics.size() << movies.size() << endl;
+
+	  for (const auto& b : books) {
+		  
+		  file << b.Name << endl;
+		  file << b.Pages << endl;
+	  }
+
+
+	  for (const auto& m : musics) {
+
+		  file << m.Name << endl;
+		  file << m.Duration << endl;
+	  }
+
+	  for (const auto& f : movies) {
+
+		  file << f.Name << endl;
+		  file << f.Duration << endl;
+	  }
+
+	  file.close();
+	  cout << "Данные сохранены в файл " << filename;
   }
 
   int main() {
 	  setlocale(LC_ALL, "ru");
+	  SetConsoleCP(1251);
+	  SetConsoleOutputCP(1251);
 
 	  char Choice;
+	  int count;
 	  vector<Book> books;
 	  vector<Music> musics;
 	  vector<Movie> movies;
+
+	  const string filename = "Library.txt";
+
 
 	  while (true) {
 		  cout << "Выберите функцию: \n B - Добавить книгу \n M - Добавить музыку \n F - Добавить фильм \n E - Выйти" << endl;
@@ -49,6 +84,8 @@ using namespace std;
 
 		  if (Choice == 'B' || Choice == 'b') {
 			
+			  cout << "Сколько книг добавить? \n";
+			  cin >> count;
 		  }
 	  }
   }
