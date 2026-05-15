8f4851b12d3a29991e976835e5c0a793f992d4da
mpeskicev7-netizen <mpeskicev7@gmail.com>
2026-05-15 04:01:50 +0300

    начал делать банк
    пока только начало, сделал то что первое приходит в голову

---
+++ Bank/Bank/Bank.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class Account {
+private:
+	int id;
+	string name;
+	
+protected:
+	double balance;
+	vector<string> history;
+};
