077c6ba473df8c342c6044773745ae96299cb3d3
mpeskicev7-netizen <mpeskicev7@gmail.com>
2026-05-23 21:41:44 +0300

    начал делать банк
    пока что вновь тока начал потому что разбирался как правильно все оформлять

--- Bank/Bank/Bank.cpp
+++ Bank/Bank/Bank.cpp
@@ -9,8 +9,19 @@ class Account {
 private:
 	int id;
 	string name;
-	
-protected:
 	double balance;
 	vector<string> history;
+
+public:
+	int getId() { return id; }
+	string getName() { return name; }
+	double getBalance() { return balance; }
+	void deposit(double amount);
+	void withdraw(double amount);
+	void addHistory(string text);
+	void printInfo();
 };
+
+void Account::deposit(double amount) {
+
+}
