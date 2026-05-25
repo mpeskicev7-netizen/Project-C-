0567dbacc680edd3478ac2ee6f10e89cb5ee3a01
mpeskicev7-netizen <mpeskicev7@gmail.com>
2026-05-25 04:01:22 +0300

    добавил конструкторы и логику методов
    сделал конструктор для аккаунта и добавил логику двух методов

--- Bank/Bank/Bank.cpp
+++ Bank/Bank/Bank.cpp
@@ -14,6 +14,12 @@ private:
 	vector<string> history;
 
 public:
+	Account(int id, string name) {
+		this -> id = id;
+		this->name = name;
+		balance = 0.0;
+	}
+
 	int getId() { return id; }
 	string getName() { return name; }
 	double getBalance() { return balance; }
@@ -29,10 +35,22 @@ class Bank {
 
 void Account::deposit(double amount) {
 
+	if (amount <= 0) {
+		cout << "Такой депозит не вносится" << endl;
+		return;
+	}
+
+	balance += amount;
 }
 
 void Account::withdraw(double amount) {
 
+	if (amount > balance) {
+		cout << "Недостаточно средств" << endl;
+		return;
+	}
+
+	balance -= amount;
 }
 
 void Account::addHistory(string text) {
@@ -55,4 +73,10 @@ int main() {
 	setlocale(LC_ALL, "ru");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+
+	char choice;
+
+	while (true) {
+	
+	}
 }
