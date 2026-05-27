167e45b6a3ee5c695d7746da1d0f435ea1329193
mpeskicev7-netizen <mpeskicev7@gmail.com>
2026-05-27 20:00:38 +0300

    многое сделал
    доделал класс банка, сделал логику многих функций, начал заполнять цикл в main()

--- Bank/Bank/Bank.cpp
+++ Bank/Bank/Bank.cpp
@@ -10,19 +10,22 @@ class Account {
 private:
 	int id;
 	string name;
+	string password;
 	double balance;
 	vector<string> history;
 
 public:
-	Account(int id, string name) {
+	Account(int id, string name, string password) {
 		this -> id = id;
 		this->name = name;
+		this->password = password;
 		balance = 0.0;
 	}
 
-	int getId() { return id; }
-	string getName() { return name; }
-	double getBalance() { return balance; }
+	int getId() const { return id; }
+	string getName() const { return name; }
+	double getBalance() const { return balance; }
+	string getPassword() const { return password; }
 	void deposit(double amount);
 	void withdraw(double amount);
 	void addHistory(string text);
@@ -30,6 +33,21 @@ public:
 };
 
 class Bank {
+private:
+	vector<Account> accounts;
+	int nextId;
+
+public:
+	Bank() {
+		nextId = 1001;
+	}
+
+	void CreateAccounts(string name, string password);
+	Account* findAccount(int id);
+	Account* findAccount(string name);
+	void printAllAccounts();
+	void SaveToFile();
+	void LoadFromFile();
 
 };
 
@@ -61,11 +79,48 @@ void Account::printInfo() {
 
 }
 
-void SaveToFile() {
+void Bank::CreateAccounts(string name, string password) {
+	Account acc(nextId, name, password);
+	accounts.push_back(acc);
+	cout << "Аккаунт создан, ваш номер: " << nextId << endl;
+	nextId++;
+}
+
+Account* Bank::findAccount(int id) {
+
+	for (int i = 0; i < accounts.size(); i++) {
+		if (accounts[i].getId() == id) {
+			return &accounts[i];
+		}
+	}
+
+	return nullptr;
+}
+
+void Bank::printAllAccounts() {
+
+	for (const auto& a : accounts) {
+
+		cout << "Имя: " << a.getName() << "Номер: " << a.getId() << endl;
+	}
+}
+
+Account* Bank::findAccount(string name) {
+
+	for (int i = 0; i < accounts.size(); i++) {
+		if (accounts[i].getName() == name) {
+			return &accounts[i];
+		}
+	}
+
+	return nullptr;
+}
+
+void Bank::SaveToFile() {
 
 }
 
-void LoadFromFile() {
+void Bank::LoadFromFile() {
 
 }
 
@@ -75,8 +130,22 @@ int main() {
 	SetConsoleOutputCP(1251);
 
 	char choice;
+	double amount;
+	string Inputname, Inputpassword;
 
 	while (true) {
+
+		cout << "Выберите операцию: " << endl;
+		cout << "A - авторизация \n";
+
+		cin >> choice;
+
+		if (choice == 'A' || choice == 'a') {
+			cout << "Введите имя" << endl;
+			cin >> Inputname;
+			cout << "Введите пароль" << endl;
+			cin >> Inputpassword;
+		}
 	
 	}
 }
