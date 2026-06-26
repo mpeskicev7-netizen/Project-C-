6ee5b409920b07a2fe2ae0e8a1bb728991dae2c0
mpeskicev7-netizen <mpeskicev7@gmail.com>
2026-06-26 08:19:38 +0300

    пуп

--- Сайт/user.py
+++
@@ -1,7 +0,0 @@
-from kaif import db, kaif, User
-
-with kaif.app_context():
-    db.create_all()
-    user = User.query.filter_by(id=1).first()
-    user.email = 'popa@gmail.com'
-    db.session.commit()
