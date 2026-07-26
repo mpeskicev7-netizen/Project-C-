f2d41c613355c1801efbaa1e85938ddfaacc4a70
mpeskicev7-netizen <mpeskicev7@gmail.com>
2026-07-26 15:48:29 +0300

    минус

--- Сайт/seed.py
+++
@@ -1,7 +0,0 @@
-from kaif import db, kaif, Bikes
-
-with kaif.app_context():
-    db.create_all()
-    bike1 = Bikes(name="Monster M2", description="Запас хода 60-80 км, мощность 250-500 Вт", price_week=3500, quantity=10)
-    db.session.add(bike1)
-    db.session.commit()
