0850513c8ae23109d802d4e198cb1d829ad5694e
mpeskicev7-netizen <mpeskicev7@gmail.com>
2026-08-16 23:01:46 +0300

    сайт

--- Заказ/user.py
+++
@@ -1,9 +0,0 @@
-from order import db, order, User
-from werkzeug.security import generate_password_hash
-
-with order.app_context():
-    db.create_all()
-    password = generate_password_hash('Macs1349500')
-    admin = User(id = 1, email = 'mpeskicev7@gmail.com', password_hash = password, name = 'Admin', is_admin = True)
-    db.session.add(admin)
-    db.session.commit()
